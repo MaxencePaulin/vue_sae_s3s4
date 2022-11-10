ad253e5b974d3e53be9dc04d862b7f35c22e67f2
Baptiste LAVAL <baptiste.laval@edu.univ-fcomte.fr>
2022-11-10 14:20:14 +0100

    init home page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <h1> Test </h1>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomePageView.vue
@@ -0,0 +1,18 @@
+<template>
+  <HomePage />
+</template>
+
+<script>
+import HomePage from '../components/HomePage'
+export default {
+  name: "HomePageView",
+
+  components: {
+    HomePage,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
