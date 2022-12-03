929c64f400d4bd24bb82787c7c3c983805fe605b
MaxencePaulin <maxence.paulin25@gmail.com>
2022-12-03 15:44:54 +0100

    ajout d'une route login

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login">
+        <h1>Connexion</h1>
+        <form>
+            <label for="username">Nom d'utilisateur</label>
+            <input type="text" id="username" name="username" v-model="username" />
+            <label for="password">Mot de passe</label>
+            <input type="password" id="password" name="password" v-model="password" />
+            <button type="submit" @click.prevent="login">Se connecter</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LoginForm"
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,34 +2,40 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import HomePage from '../views/HomePageView'
+import LoginView from '../views/LoginView'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: "/home",
-    name: "homepage",
-    component: HomePage
-  }
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    },
+    {
+        path: "/home",
+        name: "homepage",
+        component: HomePage
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: LoginView
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
