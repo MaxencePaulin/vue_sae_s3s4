0675f8ce846478ee0fe467d901a40cc836a6e344
MaxencePaulin <maxence.paulin@edu.univ-fcomte.fr>
2022-12-06 14:44:03 +0100

    suppression des trucs de baptiste au début

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import HomePage from '../views/HomePageView'
 import LoginView from '../views/LoginView'
 import RegisterView from '../views/RegisterView'
 import AdminView from '../views/AdminView'
@@ -25,11 +24,6 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    },
-    {
-        path: "/home",
-        name: "homepage",
-        component: HomePage
     },
     {
         path: "/login",
