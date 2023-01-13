6e68240db5d24955f7935c578b0ddd029cd2cc7c
MaxencePaulin <maxence.paulin@edu.univ-fcomte.fr>
2023-01-13 13:35:54 +0100

    planning fini

--- src/views/PlanningView.vue
+++ src/views/PlanningView.vue
@@ -2,67 +2,69 @@
   <v-container fluid>
     <v-row>
       <v-col cols="12">
-          <button name="button" class="button1" v-on:click="showTable1 = true, showTable2 = false, showTable3 = false, showTable4 = false">Jour 1</button>
-          <button name="button" class="button1" v-on:click="showTable1 = false, showTable2 = true, showTable3 = false, showTable4 = false">Jour 2</button>
-          <button name="button" class="button1" v-on:click="showTable1 = false, showTable2 = false, showTable3 = true, showTable4 = false">Jour 3</button>
-          <button name="button" class="button1" v-on:click="showTable1 = false, showTable2 = false, showTable3 = false, showTable4 = true">Jour 4</button>
+          <div style="display: flex;align-items:center;justify-content: center">
+              <v-btn color="amber" class="mx-6 mt-12 mb-12" @click="status = 1">Jour 1</v-btn>
+              <v-btn color="amber" class="mx-6 mt-12 mb-12" @click="status = 2">Jour 2</v-btn>
+              <v-btn color="amber" class="mx-6 mt-12 mb-12" @click="status = 3">Jour 3</v-btn>
+              <v-btn color="amber" class="mx-6 mt-12 mb-12" @click="status = 4">Jour 4</v-btn>
+              <v-btn color="amber" class="mx-6 mt-12 mb-12" @click="status = 5">Jour 5</v-btn>
+          </div>
         <div class="center">
-          <table v-if="showTable1">
-            <thead>
-            <tr>
-              <th>Artiste</th>
-              <th>Heure de passage</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(artist, i) in table1Data" :key="i">
-              <td>{{ artist.name }}</td>
-              <td>{{ artist.time }}</td>
-            </tr>
-            </tbody>
-          </table>
-          <table v-if="showTable2">
-            <thead>
-            <tr>
-              <th>Artiste</th>
-              <th>Heure de passage</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(artist, i) in table2Data" :key="i">
-              <td>{{ artist.name }}</td>
-              <td>{{ artist.time }}</td>
-            </tr>
-            </tbody>
-          </table>
-          <table v-if="showTable3">
-            <thead>
-            <tr>
-              <th>Artiste</th>
-              <th>Heure de passage</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(artist, i) in table3Data" :key="i">
-              <td>{{ artist.name }}</td>
-              <td>{{ artist.time }}</td>
-            </tr>
-            </tbody>
-          </table>
-          <table v-if="showTable4">
-            <thead>
-            <tr>
-              <th>Artiste</th>
-              <th>Heure de passage</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="(artist, i) in table4Data" :key="i">
-              <td>{{ artist.name }}</td>
-              <td>{{ artist.time }}</td>
-            </tr>
-            </tbody>
-          </table>
+            <div  v-if="status === 1">
+                <h1 style="text-align: center;color:#FFC107;margin-bottom: 5vh">Samedi 22 juillet 2023</h1>
+                <v-data-table
+                    :headers="headers"
+                    :items="table1Data"
+                    :sort-by="['time']"
+                    :sort-desc="[true]"
+                    class="elevation-1"
+                    dark
+                ></v-data-table>
+            </div>
+            <div  v-else-if="status === 2">
+                <h1 style="text-align: center;color:#FFC107;margin-bottom: 5vh">Dimanche 23 juillet 2023</h1>
+                <v-data-table
+                    :headers="headers"
+                    :items="table2Data"
+                    :sort-by="['time']"
+                    :sort-desc="[true]"
+                    class="elevation-1"
+                    dark
+                ></v-data-table>
+            </div>
+            <div  v-else-if="status === 3">
+                <h1 style="text-align: center;color:#FFC107;margin-bottom: 5vh">Lundi 24 juillet 2023</h1>
+                <v-data-table
+                    :headers="headers"
+                    :items="table3Data"
+                    :sort-by="['time']"
+                    :sort-desc="[true]"
+                    class="elevation-1"
+                    dark
+                ></v-data-table>
+            </div>
+            <div  v-else-if="status === 4">
+                <h1 style="text-align: center;color:#FFC107;margin-bottom: 5vh">Mardi 25 juillet 2023</h1>
+                <v-data-table
+                    :headers="headers"
+                    :items="table4Data"
+                    :sort-by="['time']"
+                    :sort-desc="[true]"
+                    class="elevation-1"
+                    dark
+                ></v-data-table>
+            </div>
+            <div  v-else-if="status === 5">
+                <h1 style="text-align: center;color:#FFC107;margin-bottom: 5vh">Mercredi 26 juillet 2023</h1>
+                <v-data-table
+                    :headers="headers"
+                    :items="table5Data"
+                    :sort-by="['time']"
+                    :sort-desc="[true]"
+                    class="elevation-1"
+                    dark
+                ></v-data-table>
+            </div>
         </div>
       </v-col>
     </v-row>
@@ -75,69 +77,100 @@ import { mapGetters } from 'vuex';
 export default {
     name: "PlanningView",
     data: () => ({
-        showTable1: true,
-        showTable2: false,
-        showTable3: false,
-        showTable4: false,
-        // table1Data: [
-        //   {name: 'Artiste 1', time: '20h00'},
-        //   {name: 'Artiste 2', time: '21h00'},
-        //   {name: 'Artiste 3', time: '22h00'}
-        // ],
-        table2Data: [
-          {name: 'Artiste 4', time: '19h00'},
-          {name: 'Artiste 5', time: '20h30'},
-          {name: 'Artiste 6', time: '22h15'}
-        ],
-        table3Data: [
-          {name: 'Artiste 7', time: '19h00'},
-          {name: 'Artiste 8', time: '20h30'},
-          {name: 'Artiste 9', time: '22h15'}
-        ],
-        table4Data: [
-          {name: 'Artiste 10', time: '19h00'},
-          {name: 'Artiste 11', time: '20h30'},
-          {name: 'Artiste 12', time: '22h15'}
+        status: 1,
+        headers: [
+            {
+                text: 'Artistes',
+                align: 'start',
+                sortable: true,
+                value: 'name',
+            },
+            { text: 'Scene', value: 'scene' },
+            { text: 'Heure de passage', value: 'time' },
         ],
     }),
     computed: {
         ...mapGetters('concert', ['allConcerts']),
         table1Data() {
-                let table1Data = [];
-                for (let i = 0; i < this.allConcerts.length; i++) {
-                    if (this.allConcerts[i].date_concert.startsWith() === '22/07/2023') { // conditions marche pas
-                        table1Data.push(this.allConcerts[i].date_concert.slice(11));
-                    }
+            let table1Data = [];
+            for (let i = 0; i < this.allConcerts.length; i++) {
+                if (this.allConcerts[i].date_concert.startsWith('22/07/2023')) {
+                    table1Data.push({
+                        time: this.allConcerts[i].date_concert.slice(11),
+                        name: this.allConcerts[i].artist.name,
+                        scene: this.allConcerts[i].scene.libelle_scene,
+                        id_artist: this.allConcerts[i].artist.id_artist,
+                        id_scene: this.allConcerts[i].scene.id_scene,
+                    });
                 }
-                return table1Data;
-            },
+            }
+            return table1Data;
+        },
+        table2Data() {
+            let table2Data = [];
+            for (let i = 0; i < this.allConcerts.length; i++) {
+                if (this.allConcerts[i].date_concert.startsWith('23/07/2023')) {
+                    table2Data.push({
+                        time: this.allConcerts[i].date_concert.slice(11),
+                        name: this.allConcerts[i].artist.name,
+                        scene: this.allConcerts[i].scene.libelle_scene,
+                        id_artist: this.allConcerts[i].artist.id_artist,
+                        id_scene: this.allConcerts[i].scene.id_scene,
+                    });
+                }
+            }
+            return table2Data;
+        },
+        table3Data() {
+            let table3Data = [];
+            for (let i = 0; i < this.allConcerts.length; i++) {
+                if (this.allConcerts[i].date_concert.startsWith('24/07/2023')) {
+                    table3Data.push({
+                        time: this.allConcerts[i].date_concert.slice(11),
+                        name: this.allConcerts[i].artist.name,
+                        scene: this.allConcerts[i].scene.libelle_scene,
+                        id_artist: this.allConcerts[i].artist.id_artist,
+                        id_scene: this.allConcerts[i].scene.id_scene,
+                    });
+                }
+            }
+            return table3Data;
+        },
+        table4Data() {
+            let table4Data = [];
+            for (let i = 0; i < this.allConcerts.length; i++) {
+                if (this.allConcerts[i].date_concert.startsWith('25/07/2023')) {
+                    table4Data.push({
+                        time: this.allConcerts[i].date_concert.slice(11),
+                        name: this.allConcerts[i].artist.name,
+                        scene: this.allConcerts[i].scene.libelle_scene,
+                        id_artist: this.allConcerts[i].artist.id_artist,
+                        id_scene: this.allConcerts[i].scene.id_scene,
+                    });
+                }
+            }
+            return table4Data;
+        },
+        table5Data() {
+            let table5Data = [];
+            for (let i = 0; i < this.allConcerts.length; i++) {
+                if (this.allConcerts[i].date_concert.startsWith('26/07/2023')) {
+                    table5Data.push({
+                        time: this.allConcerts[i].date_concert.slice(11),
+                        name: this.allConcerts[i].artist.name,
+                        scene: this.allConcerts[i].scene.libelle_scene,
+                        id_artist: this.allConcerts[i].artist.id_artist,
+                        id_scene: this.allConcerts[i].scene.id_scene,
+                    });
+                }
+            }
+            return table5Data;
+        },
     }
 }
 </script>
 
 <style>
-.button1 {
-    background-color: #FFC300;
-    border: none;
-    font-weight: bold;
-    padding: 15px 32px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 16px;
-    margin: 20px 10px 250px 20px;
-    cursor: pointer;
-}
-
-table{
-    border-collapse: collapse;
-    width: 30%;
-
-}
-
-th, td{
-    border: 1px solid black;
-}
 
 .center{
   display: flex;
