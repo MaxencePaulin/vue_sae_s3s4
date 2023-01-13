a7016dd055db15a1972eae5049f2646525dc975a
Tolmeh <anto.lachat@gmail.com>
2023-01-13 10:38:00 +0100

    Planning v1

--- src/views/PlanningView.vue
+++ src/views/PlanningView.vue
@@ -1,67 +1,55 @@
-<template> 
-    <div id="planninTable">
-        <table style="border: black solid 2px; border-collapse: collapse; width: 75%; text-align: center;">
-            <tr style="background-color: #ffffff;">
-              <th style="border: black solid 1px; width: 25%;"></th>
-              <th style="border: black solid 1px;">Samedi</th>
-              <th style="border: black solid 1px;">Dimanche</th>
-              <th style="border: black solid 1px;">Lundi</th>
-              <th style="border: black solid 1px;">Mardi</th>
-            </tr>
-            <tr>
-                <td class="artist" style="border: black solid 1px;">Scene</td>
-                <td class="artist" style="border: black solid 1px;">Artiste</td>
-                <td class="artist" style="border: black solid 1px;">Artiste</td>
-                <td class="artist" style="border: black solid 1px;">Artiste</td>
-                <td class="artist" style="border: black solid 1px;">Artiste</td>
-            </tr>
-            <tr>
-                <td class="artist" style="border: black solid 1px;">Scene</td>
-                <td class="artist" style="border: black solid 1px;">Artiste</td>
-                <td class="artist" style="border: black solid 1px;">Artiste</td>
-                <td class="artist" style="border: black solid 1px;">Artiste</td>
-                <td class="artist" style="border: black solid 1px;">Artiste</td>
-            </tr>
-            <tr>
-                <td class="artist" style="border: black solid 1px;">Scene</td>
-                <td class="artist" style="border: black solid 1px;">Artiste</td>
-                <td class="artist" style="border: black solid 1px;">Artiste</td>
-                <td class="artist" style="border: black solid 1px;">Artiste</td>
-                <td class="artist" style="border: black solid 1px;">Artiste</td>
-            </tr>
-            <tr>
-                <td class= "artist" style="border: black solid 1px;">Scene</td>
-                <td class= "artist" style="border: black solid 1px;">Artiste</td>
-                <td class= "artist" style="border: black solid 1px;">Artiste</td>
-                <td class= "artist" style="border: black solid 1px;">Artiste</td>
-                <td class= "artist" style="border: black solid 1px;">Artiste</td>
-            </tr>
-        </table>
-    </div>
+<template>
+  <div>
+    <button v-on:click="showTable1 = true, showTable2 = false">Afficher tableau 1</button>
+    <button v-on:click="showTable1 = false, showTable2 = true">Afficher tableau 2</button>
+    <table v-if="showTable1">
+      <thead>
+      <tr>
+        <th>Artiste</th>
+        <th>Heure de passage</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(artist, i) in table1Data" :key="i">
+        <td>{{ artist.name }}</td>
+        <td>{{ artist.time }}</td>
+      </tr>
+      </tbody>
+    </table>
+    <table v-if="showTable2">
+      <thead>
+      <tr>
+        <th>Artiste</th>
+        <th>Heure de passage</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(artist, i) in table2Data" :key="i">
+        <td>{{ artist.name }}</td>
+        <td>{{ artist.time }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
-
 <script>
-import { /*mapGetters,*/ mapState } from 'vuex';
-
 export default {
-    name: "PlanningView",
-    computed: {
-        ...mapState(['artist']),
+  data() {
+    return {
+      showTable1: true,
+      showTable2: false,
+      table1Data: [
+        { name: 'Artiste 1', time: '20h00' },
+        { name: 'Artiste 2', time: '21h00' },
+        { name: 'Artiste 3', time: '22h00' }
+      ],
+      table2Data: [
+        { name: 'Artiste 4', time: '19h00' },
+        { name: 'Artiste 5', time: '20h30' },
+        { name: 'Artiste 6', time: '22h15' }
+      ]
     }
+  }
 }
 </script>
-
-<style>
-table {
-    margin: auto;
-    margin-top: 10%;
-}
-tr {
-    height: 50px;
-}
-.artist {
-    color: #ffc107
-}
-
-</style>
