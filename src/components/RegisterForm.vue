5cbd412f0ae6f663175af207e3437d99dd89b1c0
AntoinePR1 <titounet.perrin@gmail.com>
2023-04-07 14:10:48 +0200

    Ajout CSRF sur register form

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,142 +1,82 @@
 <template>
-        <v-container style="margin-bottom: 10vh; margin-top: 10vh">
-            <v-row>
-                <v-col cols="12"  sm="10" md="8" offset-md="2" offset-sm="1" >
-                    <v-card style="background-color: rgb(255,222,89)">
-                        <v-card-title>
-                            <h1 class="display-2 mx-auto">Inscription</h1>
-                        </v-card-title>
-                        <v-card-text>
-                            <v-form ref="form" v-model="valid" lazy-validation>
-                                <v-row>
-                                    <v-col
-                                        cols="12"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                            v-model="identifiant"
-                                            :rules="identifiantRules"
-                                            label="Identifiant*"
-                                            required
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                            v-model="password"
-                                            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                                            :rules="passwordRules"
-                                            :type="showPassword ? 'text' : 'password'"
-                                            label="Mot de passe*"
-                                            required
-                                            @click:append="showPassword = !showPassword"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                            v-model="confirmPassword"
-                                            :append-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                                            :rules="confirmPasswordRules"
-                                            :type="showConfirmPassword ? 'text' : 'password'"
-                                            label="Confirmer le mot de passe*"
-                                            required
-                                            @click:append="showConfirmPassword = !showConfirmPassword"
-                                        ></v-text-field>
-                                    </v-col>
-                                </v-row>
-                                <v-row>
-                                    <v-col
-                                        cols="12"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                            v-model="email"
-                                            :rules="emailRules"
-                                            label="Email*"
-                                            required
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                            v-model="firstname"
-                                            :rules="firstnameRules"
-                                            label="Prénom*"
-                                            required
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                            v-model="lastname"
-                                            :rules="lastnameRules"
-                                            label="Nom*"
-                                            required
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                            v-model="dob"
-                                            :rules="dobRules"
-                                            label="Date de naissance"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                            v-model="genre"
-                                            :rules="genreRules"
-                                            label="Genre (homme, femme ou autre)"
-                                        ></v-text-field>
-                                    </v-col>
-                                    <v-col
-                                        cols="12"
-                                        md="4"
-                                    >
-                                        <v-text-field
-                                            v-model="mobile"
-                                            :rules="mobileRules"
-                                            label="Numéro de téléphone"
-                                        ></v-text-field>
-                                    </v-col>
-                                </v-row>
-                                <v-text-field
-                                    v-model="address"
-                                    :rules="addressRules"
-                                    label="Adresse"
-                                ></v-text-field>
-                            </v-form>
-                        </v-card-text>
-                        <v-card-actions>
-                            <span>Les champs marquées par des "*" sont obligatoires</span>
-                            <v-spacer></v-spacer>
-                            <v-btn color="warning" @click="register">S'inscrire</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-col>
-            </v-row>
-        </v-container>
+    <v-container style="margin-bottom: 10vh; margin-top: 10vh">
+        <v-row>
+            <v-col cols="12" sm="10" md="8" offset-md="2" offset-sm="1">
+                <v-card style="background-color: rgb(255,222,89)">
+                    <v-card-title>
+                        <h1 class="display-2 mx-auto">Inscription</h1>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-form ref="form" v-model="valid" lazy-validation>
+                            // CSRF token
+                            <input type="hidden" name="_csrf" :value="csrfToken">
+                            <v-row>
+                                <v-col cols="12" md="4">
+                                    <v-text-field v-model="identifiant" :rules="identifiantRules" label="Identifiant*"
+                                        required></v-text-field>
+                                </v-col>
+                                <v-col cols="12" md="4">
+                                    <v-text-field v-model="password" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                                        :rules="passwordRules" :type="showPassword ? 'text' : 'password'"
+                                        label="Mot de passe*" required
+                                        @click:append="showPassword = !showPassword"></v-text-field>
+                                </v-col>
+                                <v-col cols="12" md="4">
+                                    <v-text-field v-model="confirmPassword"
+                                        :append-icon="showConfirmPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                                        :rules="confirmPasswordRules" :type="showConfirmPassword ? 'text' : 'password'"
+                                        label="Confirmer le mot de passe*" required
+                                        @click:append="showConfirmPassword = !showConfirmPassword"></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <v-row>
+                                <v-col cols="12" md="4">
+                                    <v-text-field v-model="email" :rules="emailRules" label="Email*"
+                                        required></v-text-field>
+                                </v-col>
+                                <v-col cols="12" md="4">
+                                    <v-text-field v-model="firstname" :rules="firstnameRules" label="Prénom*"
+                                        required></v-text-field>
+                                </v-col>
+                                <v-col cols="12" md="4">
+                                    <v-text-field v-model="lastname" :rules="lastnameRules" label="Nom*"
+                                        required></v-text-field>
+                                </v-col>
+                                <v-col cols="12" md="4">
+                                    <v-text-field v-model="dob" :rules="dobRules" label="Date de naissance"></v-text-field>
+                                </v-col>
+                                <v-col cols="12" md="4">
+                                    <v-text-field v-model="genre" :rules="genreRules"
+                                        label="Genre (homme, femme ou autre)"></v-text-field>
+                                </v-col>
+                                <v-col cols="12" md="4">
+                                    <v-text-field v-model="mobile" :rules="mobileRules"
+                                        label="Numéro de téléphone"></v-text-field>
+                                </v-col>
+                            </v-row>
+                            <v-text-field v-model="address" :rules="addressRules" label="Adresse"></v-text-field>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <span>Les champs marquées par des "*" sont obligatoires</span>
+                        <v-spacer></v-spacer>
+                        <v-btn color="warning" @click="register">S'inscrire</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
-import Vue from 'vue'
+import Vue from 'vue';
+import axios from 'axios';
+import helmet from 'helmet';
 export default {
     name: "RegisterForm",
     data: () => ({
+        //CSRF
+        csrfToken: '',
         valid: true,
         identifiant: "",
         password: "",
@@ -191,15 +131,25 @@ export default {
             (v) => !v || /^(homme|femme|autre|male|female|other)$/.test(v.toLowerCase()) || "Le genre doit être homme, femme ou autre (male, female or other)"
         ],
     }),
+
+    mounted () {
+        this.csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    },
     methods: {
         register() {
+            // Créer un jeton CSRF
+            const csrfToken = helmet.csrfToken();
+
+            // Ajouter le jeton CSRF à l'en-tête de la requête
+            axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
+
             if (this.login === '' ||
                 this.password === '' ||
                 this.password !== this.confirmPassword) {
                 alert("Champs incorrect ou les mots de passe ne correspondent pas");
                 return;
             } if (this.genre !== '' && (this.genre.toLowerCase() !== "homme" && this.genre.toLowerCase() !== "femme"
-                        && this.genre.toLowerCase() !== "autre")) {
+                && this.genre.toLowerCase() !== "autre")) {
                 alert("Le genre doit être homme, femme ou autre");
                 return;
             }
@@ -215,7 +165,7 @@ export default {
                 genre: this.genre
             }).then((response) => {
                 if (response.data.success === 1) {
-                    this.$router.push({name: "login"})
+                    this.$router.push({ name: "login" })
                 } else {
                     alert("Identifiant ou email déjà utilisé");
                 }
@@ -228,6 +178,4 @@ export default {
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
